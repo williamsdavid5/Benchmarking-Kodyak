7d5120d2e03ac7033f6fadffa88a5d765c9334b5
David Duarte (Williams) <davidduart04@gmail.com>
2026-03-25 17:41:06 -0300

    estrutura de blocos definidas na página de dashborad, recharts já instalado

--- src/pages/Deashboard.jsx
+++ src/pages/Deashboard.jsx
@@ -1,7 +1,37 @@
+import './dashboard.css'
+
 export default function Dashborad() {
     return (
         <>
-            <p>dashborad</p>
+            <main className="mainDashboard">
+                <header className="superiorDashboard">
+                    <h1>Dashboard</h1>
+                    <p>Acompanhe a análise dos dados registrados com feedback visual.</p>
+                </header>
+                <section className="blocosDashborad">
+                    <div className="bloco">
+                        <p>bloco</p>
+                    </div>
+                    <div className="bloco">
+                        <p>bloco</p>
+                    </div>
+                    <div className="bloco">
+                        <p>bloco</p>
+                    </div>
+                    <div className="bloco">
+                        <p>bloco</p>
+                    </div>
+                    <div className="bloco">
+                        <p>bloco</p>
+                    </div>
+                    <div className="bloco">
+                        <p>bloco</p>
+                    </div>
+                    <div className="bloco">
+                        <p>bloco</p>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
